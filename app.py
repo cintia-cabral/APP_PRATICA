d9287b6872f9b31d8b89d2d99106ea58e5e4af9a
cintia-cabral <cintiacabral901@gmail.com>
2025-12-15 11:17:16 -0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from streamlit_option_menu import option_menu
 st.header("Indroduzindo os Elementos  do Steamlit")
 menu=option_menu(menu_title="Menu",
                  options=["Inicio", "Grafico Estatistico","Grafico Dinamico", "Widgts", "Formulario"],
-                 icons=["house", "bar_chat", "bar_chart_line","toggles","bar_chart"],
+                icons=["house", "bar-chart", "graph-up", "toggles", "bar-chart"],
                  menu_icon="cast", 
                  default_index=0,
                  orientation="horizontal"
