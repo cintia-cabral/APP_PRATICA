b95904c3292e2f8974b877f6ebd12657c9c50818
cintia-cabral <cintiacabral901@gmail.com>
2025-12-15 11:59:37 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import time
 from streamlit_option_menu import option_menu
 import re
